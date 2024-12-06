50c8d98dd6a168e92ef5c8cde0d2f99d004bca66
ElectricalBoy <15651807+ElectricalBoy@users.noreply.github.com>
2024-12-06 09:57:59 +0900

    refactor attendance code

--- attendence.py
+++ attendence.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timezone
 
 import discord
+import discord.utils
 from zoneinfo import ZoneInfo
 
 from discord.ext import commands
@@ -25,17 +26,21 @@ class Attendence(commands.Cog):
         user = init_load_user(interaction)  # 사용자 로드
 
         # 현재 시간과 출석 기록 확인
-        korea_tz = ZoneInfo("Asia/Seoul")
-        current_time_kst = datetime.now(korea_tz)  # 한국 시간으로 현재 시간 얻기
-        today = current_time_kst.date()
+        current_time_kst = discord.utils.utcnow().astimezone(
+            ZoneInfo("Asia/Seoul")
+        )  # 한국 시간으로 현재 시간 얻기
 
         # 마지막 출석 날짜 확인
-        last_attendance_date = user.login_record.astimezone(
-            ZoneInfo("Asia/Seoul")
-        ).date()
+        last_attendance_date = user.login_record.astimezone(ZoneInfo("Asia/Seoul"))
 
         # 이미 오늘 출석한 경우 처리
-        if last_attendance_date == today:
+        if all(
+            [
+                last_attendance_date.year == current_time_kst.year,
+                last_attendance_date.month == current_time_kst.month,
+                last_attendance_date.day == current_time_kst.day,
+            ]
+        ):
             await interaction.followup.send(
                 "오늘은 이미 출석했습니다. 내일 다시 출석해주세요!", ephemeral=True
             )
@@ -43,9 +48,7 @@ class Attendence(commands.Cog):
 
         # 출석 처리 및 골드 지급
         user.update_balance(config().daily_reward)  # 골드 업데이트
-        user.login_record = current_time_kst.astimezone(
-            timezone.utc
-        )  # 로그인 기록 추가
+        user.login_record = discord.utils.utcnow()  # 로그인 기록 추가
 
         # 출석 완료 메시지
         await interaction.followup.send(
